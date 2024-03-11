6d9d6ce6adc2d457a9cce3aab779dbbb4f54674a
Richard <bakobiibizo@gmail.com>
2024-03-11 05:56:43 +0000

    updated app, added users page and link

---
+++ app/users/new/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+const NewUsersPage = (props: Props) => {
+    return (
+        <div>
+            NewUsersPage
+        </div>
+    )
+}
+
+export default NewUsersPage
---
+++ app/users/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface Props {
+
+}
+const UserPage = (props: Props) => {
+    return (
+        <div>
+            <h1>User Page</h1>
+        </div>
+    )
+}
+
+export default UserPage
